fb235e769e9414b5936face819812b4231f7ea3c
Brunner Dave dbrunner <57140925+dave1b@users.noreply.github.com>
2022-05-18 16:08:56 +0200

    implemented 3 api method

--- server.js
+++ server.js
@@ -1,97 +1,110 @@
-// Initializing REST-API
-var mongoClient = require('mongodb').MongoClient;
-var express = require('express');
-var server = express();
-var bodyParser = require("body-parser");
+const mongoClient = require("mongodb").MongoClient;
+const objectId = require("mongodb").ObjectId;
+const bodyParser = require("body-parser");
+const express = require("express");
+const server = express();
+require('dotenv').config()
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({ extended: false }));
-
-// Initializing Socket
-var httpServer = require('http').Server(server);
-const io = require("socket.io")(httpServer, {
-   serveClient: false,
-   cors: {    
-      origin: "*",    
-      methods: ["GET", "POST, PUT"]  
-   }
-});
-
-// Reading .env variables
-require('dotenv').config()
-const API_KEY = process.env.SECRET_KEY;
-const CONNECTION_STRING = "mongodb://localhost:27016/";
-
-// Handling CORS-Restriction
 server.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', '*');
     if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'POST, GET, PUT');
+        res.header('Access-Control-Allow-Methods', 'POST, GET');
         return res.status(200).json({});
     }
     next();
 })
 
-// Client requesting all users
-server.get('/api/v1/listRun', async (req, res) => {
-   latestRun = await getLatestRun();
-   res.json(latestRun);
+const connectionString = `${process.env.LOCALMONGODB}`;
+
+/* 
+Method for getting a questionSet
+*/
+server.get('/api/v1/questionSet', async (req, res) => {
+    const client = await mongoClient.connect(connectionString);
+    const db = client.db('quizIT');
+    const collection = db.collection('questions');
+    var n = 5
+    var r = Math.floor(Math.random() * n);
+    const result = await collection.find({ questionSet: 1 }).toArray();
+    console.log("/api/v1/questionSet called -> result: ")
+    result.forEach(r => console.log(r))
+    if (result) {
+        res.send(result);
+    } else {
+        res.status(404);
+    }
+    res.end();
 });
 
-// Update run
-server.put('/api/v1/updateRun', async (req, res) => {
-   const run = req.body;
-   // Check validity of Key
-   if (run.apiKey != API_KEY) {
-      res.status(401);
-      res.end();
-      console.log("not authorized")
-      return;
-   }
-   delete run["apiKey"];
-   broadcastClients(run);
-   // Write to DB
-   const client = await mongoClient.connect(CONNECTION_STRING);
-   const db = client.db('pren');
-   const collection = db.collection('run');
-   const queryFilter = { id: 1 }
-   const result = await collection.updateOne(queryFilter, { $set: run });
-   // Copy to Archive if end
-   if (run.isFinished == true) {
-      const collection2 = db.collection('runArchive');
-      await collection2.insertOne(run);
-   }
-   if (result) {
-      res.send(result);
-   } else {
-      res.status(404);
-   }
-   res.end();
-   return
+/* 
+Method for getting the leaderboard.
+*/
+server.get('/api/v1/leaderboard', async (req, res) => {
+    const client = await mongoClient.connect(connectionString);
+    const db = client.db('quizIT');
+    const collection = db.collection('leaderboard');
+    const leaderboard = await collection.findOne({ status: "ok" })
+    if (leaderboard) {
+        res.send(leaderboard);
+    } else {
+        res.status(404);
+    }
+    res.end();
 });
 
-// on initial socket connection
-io.on('connection',async function(socket){
-   var latestRun = await getLatestRun();
-   broadcastClients(latestRun);
-   // Whenever someone disconnects this piece of code executed
-   socket.on('disconnect', function () {
-   });
+
+/* 
+Method for posting a new Score.
+He checks if the score is among the 10 best.
+First it looks at the score-points and then at the time.
+The higher the points and the lower the time -> the higher is the rank.
+*/
+server.post('/api/v1/score', async (req, res) => {
+    console.log("new score received: " + req.body.score)
+    const client = await mongoClient.connect(connectionString);
+    const db = client.db('quizIT');
+    const collection = db.collection('leaderboard');
+    const leaderboard = await collection.findOne({ status: "ok" })
+
+    var newScore = req.body
+    var i = 0
+    var flagNewHighScoreDetected = false
+
+    leaderboard.topTenScores.forEach(ScoreFromDB => {
+        if (ScoreFromDB == null || (newScore.score > ScoreFromDB.score && !flagNewHighScoreDetected) || (newScore.score >= ScoreFromDB.score && newScore.timeInMilliseconds < ScoreFromDB.timeInMilliseconds && !flagNewHighScoreDetected)) {
+            console.log(ScoreFromDB)
+            console.log(i)
+            mapEachScoreOneDown(leaderboard, i, newScore)
+            i++
+            flagNewHighScoreDetected = true
+            return
+        } else if (i + 1 == leaderboard.topTenScores.length && i < 10) {
+            leaderboard.topTenScores[i + 1] = newScore
+        }
+        else {
+            i++
+        }
+    });
+
+    function mapEachScoreOneDown(leaderboard, i, newScore) {
+        console.log("i: " + i)
+        console.log("length of array before " + leaderboard.topTenScores.length)
+        leaderboard.topTenScores.splice((i), 0, newScore)
+        console.log("length of array " + leaderboard.topTenScores.length)
+        leaderboard.topTenScores.splice(10, 1)
+    }
+
+    const result = await collection.updateOne({ status: "ok" }, { $set: leaderboard });
+    if (result) {
+        res.send(await collection.findOne({ status: "ok" }));
+    } else {
+        res.status(404);
+    }
+    res.end();
 });
 
-function broadcastClients(run) {
-   io.emit('runUpdated', run);
-}
 
-async function getLatestRun() {
-   const client = await mongoClient.connect(CONNECTION_STRING);
-   const db = client.db('pren');
-   const collection = db.collection('run');
-   const queryFilter = { id: 1 };
-   const result = await collection.find(queryFilter).toArray();
-   return result[0];
-}
-
-var port = 8080
-var listener = httpServer.listen(port);
+var listener = server.listen(8085);
 console.log("Server running at port " + listener.address().port)
